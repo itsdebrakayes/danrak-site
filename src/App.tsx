d11896e76d7ea912e83aa8ef29e539798120a7b1
itsdebrakayes <166943941+itsdebrakayes@users.noreply.github.com>
2025-07-28 22:33:21 -0500

    Split sections into pages and add swiper navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Showcase from "./pages/Showcase";
 import ProjectDetails from "./pages/ProjectDetails";
 import NotFound from "./pages/NotFound";
@@ -18,6 +21,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/showcase" element={<Showcase />} />
           <Route path="/project/:id" element={<ProjectDetails />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import ThemeToggle from '@/components/ThemeToggle';
+import AboutSection from '@/components/sections/AboutSection';
+
+const About = () => (
+  <div className="relative">
+    <ThemeToggle />
+    <AboutSection />
+  </div>
+);
+
+export default About;
